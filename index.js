43205813dd63c128d3bb7344c651e7cf599aa79d
Joshua K Bader <jaybshinsen@hotmail.com>
2020-04-21 00:28:05 +0800

    first push for basic code

---
+++ index.js
@@ -0,0 +1,139 @@
+const inquirer = require("inquirer");
+const fs = require('fs').promises;
+
+inquirer
+  .prompt([{
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "folder",
+      message: "What is the project folder name?",
+    },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your GitHub Email address?",
+  },
+    {
+      type: "input",
+      name: "title",
+      message: "what is the title of your Readme.Md project?",
+    },
+
+    {
+      type: "input",
+      name: "date",
+      message: "what is the date for this project",
+    },
+
+    {
+      type: "input",
+      name: "Introduction",
+      message: "can you provide a short introduction summary of your project?",
+    },
+
+    {
+      type: "checkbox",
+      name: "contents",
+      message: "which following content page heading would you like to use",
+      choices: ["Installation", "Keywords", "Feedback", "Future Additions", "Additional Information", ]
+    },
+
+    {
+      type: "input",
+      name: "installation",
+      message: "can you provide the installation procedure?",
+    },
+
+    {
+      type: "input",
+      name: "use",
+      message: "can you provide usuages for your project?",
+    },
+
+    {
+      type: "editor",
+      name: "issues",
+      message: "Please enter issues that arose with this project. To exit press 'ESC' and type ':', 'W', 'Q' then 'Enter'",
+    },
+
+    {
+      type: "input",
+      name: "licenses",
+      message: "Can you provide which licenses you are using?",
+    },
+
+    {
+      type: "input",
+      name: "contributors",
+      message: "Who contributed to your project?",
+    },
+
+  ]).then(data => {
+      const readMeInformation = `
+[h![GitHub last commit](https://img.shields.io/github/last-commit/${data.username}/${data.folder}?style=for-the-badge)](https://github.com/${data.username}/${data.folder})
+### ${data.title} 
+${data.date}
+${data.Introduction}
+  
+## ${data.contents}
+
+## Installation
+${data.installation}
+
+## Usuage
+${data.use}
+
+## Issues
+${data.issues}
+
+## Licenses
+${data.licenses}
+
+## Contributions
+${data.contributors}
+
+# ${data.name} // ${data.username}
+
+      `;
+
+     return fs.writeFile("readmetest.md", readMeInformation);
+        })
+
+  .then(() => {
+
+    console.log("Readme.Md file has been generated")
+
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompts can't be rendered in the current environment
+    } else {
+      console.error(error)
+    }
+    
+  });
+
+  
+
+  var shields = require('shields')();
+ 
+  shields('travis', {
+    repo: 'KenanY/shields'
+  });
+
+
+
+// function init() {
+
+// }
+
+// init();
---
+++ readmetest.md
@@ -0,0 +1,28 @@
+
+[h![GitHub last commit](https://img.shields.io/github/last-commit/Ryku72/HomeWorkW9?style=for-the-badge)](https://github.com/Ryku72/HomeWorkW9)
+### HomeWork Week 9: Node.Js 
+21 April 2020
+Using Node.js and a number of dependencies I have made a readme.md generator
+  
+## Installation,Keywords,Feedback,Future Additions,Additional Information
+
+## Installation
+after inputting npm install please then type node index.js
+
+## Usuage
+generate Readme.md 
+
+## Issues
+till writing some of the code for the project but not finished yet
+
+
+
+## Licenses
+MIT
+
+## Contributions
+Joshua K Bader
+
+# Joshua K Bader // Ryku72
+
+      
